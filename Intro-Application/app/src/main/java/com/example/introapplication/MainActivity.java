74a009ea753f09ea8b447b6010b6c6b30bdd8b0e
Aman Lalpuria <aman.lalpuria@juspay.in>
2019-06-01 14:29:36 +0530

    1. Created a Button 2. Click Button Event 3. Can get number of click in logs

--- Intro-Application/app/src/main/java/com/example/introapplication/MainActivity.java
+++ Intro-Application/app/src/main/java/com/example/introapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.introapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    int count =0;
+    public void buttonCLick(View buttonView){
+        count++;
+        Log.i("Button Click = ", count+"");
+//        TextView text = (TextView)findViewById((R.id.countText));
+//        text.setText(count);
     }
 }
