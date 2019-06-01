81729cdabf6bbcc0fca2e1abd98546bc483e5995
Aman Lalpuria <aman.lalpuria@juspay.in>
2019-06-01 15:17:45 +0530

    1. Added Edit Text 2. Log the entered text

--- Intro-Application/app/src/main/java/com/example/introapplication/MainActivity.java
+++ Intro-Application/app/src/main/java/com/example/introapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Button Click = ", count+"");
 //        TextView text = (TextView)findViewById((R.id.countText));
 //        text.setText(count);
+        EditText number = (EditText)findViewById(R.id.editText);
+        Log.i("Number -",number.getText().toString());
     }
 }
