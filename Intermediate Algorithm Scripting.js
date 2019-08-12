2e6f232cd22010a45d7f734570db059cc4cdb90e
Aman Lalpuria <aman.sharma163@gmail.com>
2019-08-12 11:58:40 +0530

    DNA question

--- Intermediate Algorithm Scripting.js
+++ Intermediate Algorithm Scripting.js
@@ -150,26 +150,86 @@
 // // Output
 // // ppddpday
 
-// 7. Search and Replace
-/**
- * Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+// // 7. Search and Replace
+// /**
+//  * Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 
-First argument is the sentence to perform the search and replace on.
+// First argument is the sentence to perform the search and replace on.
 
-Second argument is the word that you will be replacing (before).
+// Second argument is the word that you will be replacing (before).
 
-Third argument is what you will be replacing the second argument with (after).
+// Third argument is what you will be replacing the second argument with (after).
 
-Note
-Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
- */
+// Note
+// Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
+//  */
 
-function myReplace(str, before, after) {
-    var index = str.indexOf(before);
-    if (str[index] === str[index].toUpperCase()) { 
-      after = after.charAt(0).toUpperCase() + after.slice(1);
-    }
-    return str.replace(before, after);
-}
+// function myReplace(str, before, after) {
+//     var index = str.indexOf(before);
+//     if (str[index] === str[index].toUpperCase()) { 
+//       after = after.charAt(0).toUpperCase() + after.slice(1);
+//     }
+//     return str.replace(before, after);
+// }
  
-console.log(myReplace("Let us go to the store", "Store", "mall"));
+// console.log(myReplace("Let us go to the Store", "Store", "mall"));
+
+// // Output 
+// // Let us go to the Mall
+
+// // 8. DNA Pairing
+// /**
+//  * The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+// Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+// Return the provided character as the first element in each array.
+
+// For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+// The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+//  */
+
+// function pairElement(str) {
+//   var dna = [];
+//   for (var i = 0; i < str.length; i++) {
+//        switch (str[i]) {
+//          case 'A':
+//            dna.push(['A', 'T']);
+//            break;
+//          case 'T':
+//            dna.push(['T', 'A']);
+//            break;
+//          case 'C':
+//            dna.push(['C', 'G']);
+//            break;
+//          case 'G':
+//            dna.push(['G', 'C']);
+//            break;
+//        }
+//  }
+//  return dna;
+// }
+
+// console.log(pairElement("GCG"));
+
+// // Output
+// // [ [ 'G', 'C' ], [ 'C', 'G' ], [ 'G', 'C' ] ]
+
+// // 9. Missing letters
+// /**
+//  * Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+//  */
+
+function fearNotLetter(str) {
+  let startASCI = str.charCodeAt(0);
+  let endASCI = str.charCodeAt(str.length-1);
+  for(let i=startASCI;i<endASCI;i++){
+
+  }
+
+}
+
+fearNotLetter("abce");
