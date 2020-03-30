ead5dd973ae691ebd3d4d874432ff2cdefa774f0
Aman Lalpuria <aman.sharma163@gmail.com>
2020-03-30 20:47:54 +0530

    Chapter 2 Hello Haskell Done

--- Haskell/test.hs
+++
@@ -1,8 +0,0 @@
-sayhello :: String -> IO()
-sayhello inputString = putStrLn("Hello," ++ inputString ++ "!")
-
-square :: Integer -> Integer
-square input = input * input
-
-pieMultiplication :: (Float, Float) -> Float
-pieMultiplication (input1, input2) = pi * (input1 * input2)
