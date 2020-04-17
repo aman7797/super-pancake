9c86732ed1e801145c1f59b3eb2a66ddd6abc8b2
Aman Lalpuria <aman.sharma163@gmail.com>
2020-04-17 09:32:12 +0530

    Haskell

--- Haskell/04. Basic Datatypes.hs
+++ Haskell/04. Basic Datatypes.hs
@@ -9,3 +9,8 @@ isPalindrome x = x == reverse(x)
 
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
 f a b = ((snd(a),snd(b)),(fst(a),fst(b)))
+
+myAbs :: Integer -> Integer
+myAbs x = if x < 0
+    then x * (-1)
+    else x
---
+++ Haskell/test.hs
@@ -0,0 +1,4 @@
+myAbs :: Integer -> Integer
+myAbs x = if x < 0
+    then x * (-1)
+    else x
