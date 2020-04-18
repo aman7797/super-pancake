9dacce511aca2287f40ecf7c97b4c8019e002dbe
Aman Lalpuria <aman.sharma163@gmail.com>
2020-04-18 09:55:49 +0530

    Type in Haskell

--- Haskell/test.hs
+++ Haskell/test.hs
@@ -2,3 +2,15 @@ myAbs :: Integer -> Integer
 myAbs x = if x < 0
     then x * (-1)
     else x
+
+id :: a -> a 
+id a = a
+
+id2 :: a -> a -> a
+id2 a _ = a
+
+id2' :: a -> a -> a
+id2' _  a = a
+
+id3 :: a -> b -> b
+id3 _ b = b
