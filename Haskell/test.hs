de22f9a408f5e484d563c471bb04963177063370
Aman Lalpuria <aman.sharma163@gmail.com>
2020-04-21 15:06:28 +0530

    1. Exercise Variety Pack 2. Exercise Case Practice

--- Haskell/test.hs
+++ Haskell/test.hs
@@ -19,5 +19,31 @@ mflip = \f -> \x -> \y -> f y x
 
 y = 1 
 x = 2 
-
 f y x = x + y
+
+
+-- functionC x y = if (x > y) then x else y
+
+functionC x y =
+    case check of
+        True -> x
+        False -> y
+    where check = x > y
+
+-- ifEvenAdd2 n = if even n then (n+2) else n
+
+ifEvenAdd2 n =
+    case even n of
+        True -> n + 2
+        False -> n
+    
+-- nums x =
+--     case compare x 0 of
+--         LT -> -1
+--         GT -> 1
+
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        EQ -> 0
