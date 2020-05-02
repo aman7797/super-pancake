4d6ad1e86641b5e945e4b420793b7f7e6f48fd70
Aman Lalpuria <aman.sharma163@gmail.com>
2020-05-02 18:17:26 +0530

    Exercise

--- Haskell/test.hs
+++ Haskell/test.hs
@@ -38,10 +38,78 @@ countTheBeforeVowel input
 
 vowelCheck :: Char -> Bool
 vowelCheck ' ' = False
-vowelCheck input = elem input ['a','e','i','o','u']
+vowelCheck input = elem input "aeiou"
 
 countVowels :: String -> Integer
 countVowels [] = 0
 countVowels (x:xa) = if vowelCheck x
     then 1 + countVowels xa
     else countVowels xa
+
+countConsonants :: String -> Integer
+countConsonants [] = 0
+countConsonants (x:xa) = if vowelCheck x
+    then countConsonants xa
+    else 1 + countConsonants xa
+
+newtype Word' = Word' String deriving (Eq, Show)
+
+vowels = "aeiou"
+
+mkWord :: String -> Maybe Word'
+mkWord input = if (countVowels input) == (countConsonants input)
+    then Just $ Word' input
+    else Nothing
+
+data Nat = Zero | Succ Nat deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0 
+natToInteger (Succ x) = 1 + natToInteger x 
+
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat input
+    | input >= 0 = Just $ integerToNat' input
+    | otherwise = Nothing
+
+integerToNat' :: Integer -> Nat
+integerToNat' 0 = Zero
+integerToNat' input = Succ (integerToNat' (input-1))
+
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust Nothing = False
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing (Just _) = False
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee _ f (Just a) = f a              -- function will return from aTob
+mayybee b _ Nothing = b                 -- return type is b that's why directly return b
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe a Nothing = a
+fromMaybe _ (Just a) = a
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe input = Just $ head input
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just input) = [input]
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes [] = []
+catMaybes (Just x:xa) = x : catMaybes xa
+catMaybes (Nothing : xa) = catMaybes xa
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe [] = Just []
+flipMaybe (Nothing:xa) = Nothing
+flipMaybe (Just x:xs) =
+   case flipMaybe xs of
+    Nothing -> Nothing
+    Just ys -> Just (x:ys)
